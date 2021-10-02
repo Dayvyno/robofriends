52ff9aa606482ccf4f84e128e7a60f9eebf6cb61
unknown <you@example.com>
2021-10-02 06:22:33 +0100

    Added service worker

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import { Provider} from 'react-redux';
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import ReactDOM from 'react-dom';
@@ -23,4 +24,6 @@ document.getElementById('root'));
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+serviceWorkerRegistration.unregister();
+
 reportWebVitals();
